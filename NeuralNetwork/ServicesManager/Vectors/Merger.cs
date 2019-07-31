9c1765fe04e455fde168af7064d6c067e2a4338d
Андрей <ande5@mail.ru>
2019-07-31 13:17:24 +0300

    Add vectorSum method in EnumerabelExtensions

--- NeuralNetwork/ServicesManager/Vectors/Merger.cs
+++ NeuralNetwork/ServicesManager/Vectors/Merger.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using NeuralNetwork.Extensions;
 
@@ -9,26 +8,23 @@ namespace NeuralNetwork.ServicesManager.Vectors
     {
         public List<List<double[]>> MergeItems(List<double[]> inputDataSets, List<double[]> outputDataSets)
         {
-            List<double[]> newInputDataSets = new List<double[]>();
-            List<double[]> newOutputDataSets = new List<double[]>();
+            var newInputDataSets = new List<double[]>();
+            var newOutputDataSets = new List<double[]>();
 
             // Все индексы слов, которые повторяются:
-            List<int> repeatedWordsIndexes = new List<int>();
-            List<double[]> indexesList = new List<double[]>();
-            int index;
+            var repeatedWordsIndexes = new List<int>();
 
-            for(int i = 0; i < inputDataSets.Count; i++)
+
+            for (int i = 0, index = 0; i < inputDataSets.Count; i++, index = 0)
             {
-                index = 0;
                 // Поиск индексов всех повторяющихся элементов:
+
                 while (index != -1)
                 {
-                   
-                    //index = FindLastIndex(inputDataSets, inputDataSets[i], i);
-           
-                     index = inputDataSets.FindIndex(inputDataSets[i],i);
-
-                    if (index != -1 && index != i)  // Если что-то найдено и найденный элемент не является обрабатываемым
+                    index = inputDataSets.FindIndex(inputDataSets[i], i);
+                    
+                    // Если что-то найдено и найденный элемент не является обрабатываемым
+                    if (index != -1 && index != i) 
                     {
                         repeatedWordsIndexes.Add(index);
                         inputDataSets.RemoveAt(index);
@@ -40,90 +36,26 @@ namespace NeuralNetwork.ServicesManager.Vectors
                 }
 
                 // Слияние выходных векторов всех найденых элементов:
-                double[] outputVector = new Double[outputDataSets[0].Length];
+                var outputVector = new double[outputDataSets[0].Length];
                 if (repeatedWordsIndexes.Count != 0)
                 {
                     outputVector = repeatedWordsIndexes.Aggregate(outputVector,
-                        (current, t) => VectorSum(current, outputDataSets[t]));
-                }
-
-                try
-                {
-                    outputVector = VectorSum(outputVector, outputDataSets[i]);
-
-                    if (repeatedWordsIndexes.Count == 0)
-                    {
-                        Console.Write("");
-                    }
-                }
-                catch
-                {
-                    return new List<List<double[]>>() {newInputDataSets, newOutputDataSets};
+                        (current, t) => current.VectorSum(outputDataSets[t]));
                 }
 
                 // Добавление уже уникального элемента в общии коллекции:
                 newInputDataSets.Add(inputDataSets[i]);
-                if (repeatedWordsIndexes.Count != 0)
-                {
-                    newOutputDataSets.Add(outputVector);
-                }
-                else
-                {
-                    newOutputDataSets.Add(outputDataSets[i]);
-                }
+                newOutputDataSets.Add(repeatedWordsIndexes.Count != 0
+                    ? outputVector.VectorSum(outputDataSets[i]): outputDataSets[i]);
 
                 // Удаление повторяющихся элементов:
-                for (int c = 0; c < repeatedWordsIndexes.Count; c++)
-                {
-                    //inputDataSets.RemoveAt(repeatedWordsIndexes[c]);
-                    outputDataSets.RemoveAt(repeatedWordsIndexes[c]);
-                }
+                foreach (var repeatedWordsIndex in repeatedWordsIndexes)
+                    outputDataSets.RemoveAt(repeatedWordsIndex);
+
+                repeatedWordsIndexes.Clear();
             }
 
-            return new List<List<double[]>>() { newInputDataSets, newOutputDataSets };
-        }
-
-        private int FindLastIndex(List<double[]> inputDataSets, double[] item, int itemIndex)
-        {
-            int index = -1;
-            
-            for (int i = 0; i < inputDataSets.Count; i++)
-            {
-                bool s = false;
-
-                if (itemIndex != i)
-                {
-                    for (int k = 0; k < item.Length; k++)
-                    {
-                        if (inputDataSets[i][k].GetHashCode() == item[k].GetHashCode())
-                        {
-                            s = true;
-                        }
-                        else
-                        {
-                            s = false;
-                            break;
-                        }
-                    }
-                }
-
-                if (s)
-                {
-                    return i;
-                }
-            }
-
-            return index;
-        }
-
-        private double[] VectorSum(double[] outputVector, double[] outputDataSet)
-        {
-            for (int i = 0; i < outputVector.Length; i++)
-            {
-                outputVector[i] += outputDataSet[i];
-            }
-
-            return outputVector;
+            return new List<List<double[]>> { newInputDataSets, newOutputDataSets };
         }
     }
 }
