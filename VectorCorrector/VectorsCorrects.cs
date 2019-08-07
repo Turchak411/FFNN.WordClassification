09d22516c0c73a081938817f08e8fb7eb44b5f3a
Андрей <ande5@mail.ru>
2019-08-07 12:53:51 +0300

    Add CorrectReductionVectors

---
+++ VectorCorrector/VectorsCorrects.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorCorrector
+{
+    public class VectorsCorrects
+    {
+        public void CorrectVectors(ref List<double[]> vectors)
+        {
+            foreach (var vector in vectors)
+            {
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    if (vector[i] > 1)
+                    {
+                        vector[i] = 1;
+                    }
+                }
+            }
+        }
+
+
+        public List<double[]> CorrectReductionVectors(List<double[]> vectors, int offsetStart, int offsetEnd)
+        {
+            var newVectors = new List<double[]>();
+            vectors.ForEach(vector =>
+            {
+                var vec = new double[offsetEnd];
+                for (int k = offsetStart, i = 0; k < offsetEnd; k++, i++)
+                {
+                    vec[i] = vector[k];
+                }
+
+                newVectors.Add(vec);
+            });
+
+            return newVectors;
+        }
+
+    }
+}
