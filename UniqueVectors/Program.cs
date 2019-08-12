20886995be8dce650b16c4b80f1b1794d62c9605
Андрей <ande5@mail.ru>
2019-08-13 01:47:10 +0300

    Add ShowTime

--- UniqueVectors/Program.cs
+++ UniqueVectors/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using UniqueVectors.ServicesManager;
 
@@ -10,13 +11,22 @@ namespace UniqueVectors
         static void Main(string[] args)
         {
             //var fileManager = new FileManager();
+            Console.WriteLine("\n>>> Start Processing >>>");
+            var stopWatch = Stopwatch.StartNew();
+
             var searchUniqueVectors = new SearchUniqueVectors();
+
+            Console.WriteLine("\n>>> Read Data Sets >>>");
+            ShowTime(stopWatch.Elapsed);
 
             var inputDataSets = LoadDataSet("inputSets.txt");
             var outputDataSets = LoadDataSet("outputSets.txt");
 
+            Console.WriteLine("\n>>> Start Processing UniqueVectors >>>");
+            ShowTime(stopWatch.Elapsed);
+
             var dataSets = searchUniqueVectors.CreateDataSets(inputDataSets, outputDataSets);
-            var listDataSets = searchUniqueVectors.CheckUniqueVectorsParallel(dataSets, "inputSets.txt",15);
+            var listDataSets = searchUniqueVectors.CheckUniqueVectorsParallel(dataSets, "inputSets.txt",2);
 
             var inputSets = new List<float[]>();
             var outputSets = new List<float[]>();
@@ -27,11 +37,19 @@ namespace UniqueVectors
                 outputSets.Add(dataSet.Ideals);
             }
 
+            Console.WriteLine("\n>>> Save UniqueVectors >>>\n");
+            ShowTime(stopWatch.Elapsed);
+
             SaveVectors(inputSets, "inputSetsNew.txt");
             SaveVectors(outputSets, "outputSetsNew.txt");
 
+            ShowTime(stopWatch.Elapsed);
+            Console.WriteLine("\n<<< Stop Processing <<<");
             Console.ReadKey();
         }
+
+        private static void ShowTime(TimeSpan time) =>
+            Console.WriteLine($">>> Time interval: {time.Hours:00}:{time.Minutes:00}:{time.Seconds:00}.{time.Milliseconds / 10:00} >>>");
 
         /// <summary>
         /// Сохранение векторов из dataSets
